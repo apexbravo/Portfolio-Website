4d9dd170d48456f3f303749eecb9aac2439fdefc
apexbravo <abelrevelation@gmail.com>
2022-06-15 02:32:04 +0200

    Icons slider

--- js/site.js
+++ js/site.js
@@ -1,53 +1,13 @@
 
-/*************** DOM ELEMENTS FOR NAVIGATION *********************/
-let nav = $$('#nav'),
-  myPic = $$('#jose'),
-  toggleBtn = $$('#toggle-btn');
-
-let navElement, myPicElement, toggleBtnElement;
-
-/*************** DOM ELEMENTS CLASS ******************************/
-class dmElement {
-    constructor(element , toAdd = [] , toRemove = []){
-        this.element = element;
-        this.toAdd = toAdd; 
-        this.toRemove = toRemove;
-    }
-}/*************** DOM ELEMENTS FUNCTIONS **************************/
-
-let alterClasses = (domElement, classes, action)
-if(action === 'remove'){
-    classes.array.forEach(cls => {
-        domElement.classList.remove(cls);
+document.onreadystatechange = function() {
+    let lastScrollPosition = 0;
+    const navbar = document.querySelector('.navbar');
+    window.addEventListener('scroll', function(e) {
+      lastScrollPosition = window.scrollY;
+      
+      if (lastScrollPosition > 100)
+        navbar.classList.add('nav-scroll');
+      else
+        navbar.classList.remove('nav-scroll');
     });
-}else if(action === 'add'){
-    classes.forEach(cls => {
-        domElement.classList.add(cls);
-    });
-};
-let classToggler = (...rest) => {
-    rest.forEach(obj => {
-        let {toAdd, toRemove, element} = obj;
-        if(toAdd instanceof Array ) alterClasses(element,toAdd,'add');
-        if(toRemove instanceof Array) alterClasses(element,toRemove,'remove');
-        obj=null;
-    });
-};
-
-/******************* WINDOW SCROLL FUNCTION HANDLER *****************************/
-function navHandler(){
-    if(document.body.scrollTop >25 || document.documentElement.scrollTop > 25){
-        (navElement = new dmElement(
-            nav, 
-            ['navbar-light', 'bg-white', 'shadow-sm'],
-            ['navbar-dark', 'py-4']
-        ))
-    }else{
-        (navElement = new dmElement(
-            nav,
-            ['navbar-dark', 'py-4'],
-            ['navbar-light', 'bg-white', 'shadow-sm']
-        ))
-    }
-    classToggler(navElement, myPicElement, toggleBtnElement);
-}
+  }
