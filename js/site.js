bcfab6ceda4f3e5b973defa9d4f8462f9562fff7
apexbravo <abelrevelation@gmail.com>
2022-06-24 00:23:04 +0200

    Responsive Mobile

--- js/site.js
+++ js/site.js
@@ -1,13 +1,13 @@
 
-document.onreadystatechange = function() {
-    let lastScrollPosition = 0;
-    const navbar = document.querySelector('.navbar');
-    window.addEventListener('scroll', function(e) {
-      lastScrollPosition = window.scrollY;
-      
-      if (lastScrollPosition > 100)
-        navbar.classList.add('nav-scroll');
-      else
-        navbar.classList.remove('nav-scroll');
-    });
-  }
+// document.onreadystatechange = function () {
+//   let lastScrollPosition = 0;
+//   const navbar = document.querySelector('.navbar');
+//   window.addEventListener('scroll', function (e) {
+//     lastScrollPosition = window.scrollY;
+
+//     if (lastScrollPosition > 100)
+//       navbar.classList.add('nav-scroll');
+//     else
+//       navbar.classList.remove('nav-scroll');
+//   });
+// }
